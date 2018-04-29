ce3ba7785a40ea70c0db1ad0c815c063b50fa685
mounir <homrani.mounir.isamm@gmail.com>
2018-04-29 11:12:50 +0100

    sessipn

--- src/VoitureBundle/Controller/DefaultController.php
+++ src/VoitureBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace VoitureBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class DefaultController extends Controller
@@ -77,4 +78,27 @@ class DefaultController extends Controller
         
 
     }
+        /**
+     * @Route("/sessionmanager")
+     */
+    public function sessionManagerAction()
+    {
+
+        $session = new Session();
+        // $session->start();
+        
+        // set and get session attributes
+        $session->set('name', 'Drak');
+        $session->get('name');
+        
+        // set flash messages
+        $session->getFlashBag()->add('notice', 'Profile updated');
+        
+        // retrieve messages
+        foreach ($session->getFlashBag()->get('notice', array()) as $message) {
+            echo '<div class="flash-notice">'.$message.'</div>';
+        }
+        die();
+        }
+
 }
